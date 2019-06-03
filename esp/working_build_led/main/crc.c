93b9ba1ce52210d26f757984a4e5efe362ae2928
Roudoudoux <alex.arnoux95160@gmail.com>
2019-06-03 17:04:36 +0200

    cleanning repository

--- esp/working_build_led/main/crc.c
+++ esp/working_build_led/main/crc.c
@@ -30,14 +30,14 @@ uint8_t compute_crc(uint8_t * frame, int size){
     uint8_t b7 = 0;
     int fsize = min(size, CRC_p);
     for(int i=0; i < fsize; i++){
-	b0 = b0 ^ (B0 & frame[i]);
-	b1 = b1 ^ (B1 & frame[i]);
-	b2 = b2 ^ (B2 & frame[i]);
-	b3 = b3 ^ (B3 & frame[i]);
-	b4 = b4 ^ (B4 & frame[i]);
-	b5 = b5 ^ (B5 & frame[i]);
-	b6 = b6 ^ (B6 & frame[i]);
-	b7 = b7 ^ (B7 & frame[i]);
+      b0 = b0 ^ (B0 & frame[i]);
+      b1 = b1 ^ (B1 & frame[i]);
+      b2 = b2 ^ (B2 & frame[i]);
+      b3 = b3 ^ (B3 & frame[i]);
+      b4 = b4 ^ (B4 & frame[i]);
+      b5 = b5 ^ (B5 & frame[i]);
+      b6 = b6 ^ (B6 & frame[i]);
+      b7 = b7 ^ (B7 & frame[i]);
     }
     return (b7 | b6 | b5 | b4 | b3 | b2 | b1 | b0);
 }
@@ -45,29 +45,29 @@ uint8_t compute_crc(uint8_t * frame, int size){
 void set_crc(uint8_t * frame, uint16_t size){
     int crc_size = (size / (CRC_p+1));
     if ((crc_size * (CRC_p +1)) != size) {
-	crc_size++;
+      crc_size++;
     }
     int offset = 0;
     int fsc = size - crc_size;
     while(crc_size > 0){
-	frame[size-crc_size] = compute_crc(frame + offset, fsc);
-	fsc = fsc - CRC_p;
-	crc_size--;
-	offset += CRC_p;
+      frame[size-crc_size] = compute_crc(frame + offset, fsc);
+      fsc = fsc - CRC_p;
+      crc_size--;
+      offset += CRC_p;
     }
 }
 
 int check_crc(uint8_t * frame, uint16_t size) {
-    int crc_size = (size / (CRC_p+1));
-    if ((crc_size * (CRC_p +1)) != size) {
-	crc_size++;
-    }
-    int offset = 0;
-    int fsc = size - crc_size;
-    while(crc_size > 0 && frame[size-crc_size] == compute_crc(frame + offset, fsc)){
-	fsc = fsc - CRC_p;
-	crc_size--;
-	offset += CRC_p;
-    }
-    return crc_size == 0;
+  int crc_size = (size / (CRC_p+1));
+  if ((crc_size * (CRC_p +1)) != size) {
+    crc_size++;
+  }
+  int offset = 0;
+  int fsc = size - crc_size;
+  while(crc_size > 0 && frame[size-crc_size] == compute_crc(frame + offset, fsc)){
+    fsc = fsc - CRC_p;
+    crc_size--;
+    offset += CRC_p;
+  }
+  return crc_size == 0;
 }
